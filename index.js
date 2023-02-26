e7f58f35b3a17126e2586b44888c360422b25b75
Administrator <admin@example.com>
2023-02-26 15:41:43 -0600

    second commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("*", (_req, res) => {
+  res.json({
+    message: 'Hello World!'
+  });
+ });
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
